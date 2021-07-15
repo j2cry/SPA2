78f7ff089ecc7a5b926f1ded2f282989dedf1c64
avagadro <j2cry.avagadro@gmail.com>
2021-07-15 16:47:16 +0300

    add table models classes and Shipment class

--- shipment_models.py
+++ shipment_models.py
@@ -1,13 +1,73 @@
+from collections import namedtuple
+from string import ascii_lowercase
+
 import pandas as pd
-from PyQt5 import QtWidgets
 from PyQt5.QtCore import QAbstractTableModel, Qt
-from PyQt5.QtGui import QFont
+
+# ----------------- default parameters -----------------
+BoxOptions = namedtuple('BoxOptions', 'rows columns separator')
+default_box_options = {'rows': 9,
+                       'columns': 9,
+                       'separator': 2}
+
+default_code_column = 'Код'
+default_columns = (default_code_column, 'st0', 'st1', 'st2', 'st3', 'st4')
 
 
-class ShipmentModel(QAbstractTableModel):
-    def __init__(self, df: pd.DataFrame, model_type):
+# ------------------- model classes --------------------
+class ShipmentModel:
+    def __init__(self, **kwargs):
+        if len(kwargs.keys() & {'df', 'columns'}) > 1:
+            raise ValueError('Only one keyword argument is required: df or columns.')
+
+        # parse box options
+        self.box_options = BoxOptions(*[v if k not in kwargs.keys() else kwargs.get(k)
+                                        for k, v in default_box_options.items()])
+
+        # if df is not specified generate new one with given columns
+        df = kwargs.get('df', pd.DataFrame(columns=kwargs.get('columns', default_columns)))
+        self.columns = df.columns
+        self.code_column = kwargs.get('code_column', default_code_column)
+        self.map_columns = kwargs.get('map_columns', list(ascii_lowercase[:self.box_options.columns]))
+
+        self.list_model = ShipmentListModel(df)
+        self.map_model = ShipmentMapModel(self.list_to_map(inplace=False))
+
+    def list_to_map(self, *, inplace=True):
+        """ Convert samples list (Series) to shipment map (DataFrame)"""
+        samples = self.list_model.df[self.code_column]
+        array, indexes = [], []
+        for index in range(0, samples.size, self.box_options.columns):
+            row = (index // self.box_options.columns) % self.box_options.rows + 1
+            array.append(samples.values[index:index + self.box_options.columns])
+            indexes.append(row)
+            # add separators
+            if row == self.box_options.rows:
+                array.extend([[''] * self.box_options.columns] * self.box_options.separator)
+                indexes.extend([''] * self.box_options.separator)
+
+        df = pd.DataFrame(array, index=indexes, columns=self.map_columns).fillna('')
+        if inplace:
+            self.map_model.df = df
+        else:
+            return df
+
+    def load(self, df: pd.DataFrame):
+        # if target columns was not found --> exit
+        if any([col not in df.columns for col in self.columns]):
+            return 'ERROR! Cannot find one or more required columns in selected file!'
+        self.list_model.df = df[self.columns]
+        self.list_to_map()
+
+    def box_amount(self):
+        # TODO
+        pass
+
+
+
+class AbstractDataFrameModel(QAbstractTableModel):
+    def __init__(self, df: pd.DataFrame):
         QAbstractTableModel.__init__(self)
-        self.model_type = model_type
         self._data = df
 
     def rowCount(self, parent=None):
@@ -16,26 +76,14 @@ class ShipmentModel(QAbstractTableModel):
     def columnCount(self, parent=None):
         return self._data.shape[1]
 
-    def data(self, index, role=Qt.DisplayRole):
-        if not index.isValid():
-            return
+    def headerData(self, section: int, orientation: Qt.Orientation, role: int = ...):
         if role == Qt.DisplayRole:
-            return str(self._data.iloc[index.row(), index.column()])
-
+            if orientation == Qt.Horizontal:
+                return self._data.columns[section]
+            if orientation == Qt.Vertical:
+                return str(self._data.index[section])
         if role == Qt.TextAlignmentRole:
-            # for first column in list-model set left text alignment
-            if (self.model_type == 'list') and (index.column() == 0):
-                return Qt.AlignVCenter
             return Qt.AlignCenter
-        # if (self.model_type == 'map') and (role == Qt.TextWordWrap):
-        #     return True
-        # if role == Qt.FontRole:
-        #     return QFont('Courier New')
-
-    def headerData(self, col: int, orientation: Qt.Orientation, role: int = ...):
-        if orientation == Qt.Horizontal and role == Qt.DisplayRole:
-            return self._data.columns[col]
-        return None
 
     @property
     def df(self):
@@ -44,3 +92,64 @@ class ShipmentModel(QAbstractTableModel):
     @df.setter
     def df(self, value):
         self._data = value
+
+
+class ShipmentListModel(AbstractDataFrameModel):
+    def data(self, index, role=Qt.DisplayRole):
+        if not index.isValid():
+            return
+
+        if role == Qt.DisplayRole:
+            return str(self._data.iloc[index.row(), index.column()])
+        if role == Qt.TextAlignmentRole:        # for first column in list set left text alignment
+            return Qt.AlignVCenter if index.column() == 0 else Qt.AlignCenter
+        # if role == Qt.FontRole:
+        #     return QFont('Courier New')
+
+    # def import_from(self, df: pd.DataFrame):
+    #     # if target columns was not found --> exit
+    #     if any([col not in df.columns for col in self._data.columns]):
+    #         return 'ERROR! Cannot find one or more required columns in selected file!'
+    #     self._data = df
+
+
+class ShipmentMapModel(AbstractDataFrameModel):
+    """ Model for shipment map, based on shipment list (ShipmentListModel) """
+    # def __init__(self, df: pd.DataFrame = None, **kwargs):
+        # parse box options
+        # self.box_options = BoxOptions(*[v if k not in kwargs.keys() else kwargs.get(k)
+        #                                 for k, v in default_box_options.items()])
+        # # get columns options
+        # self.code_column = kwargs.pop('code_column', default_code_column)
+        # self.columns = kwargs.pop('columns', list(ascii_lowercase[:self.box_options.columns]))
+        #
+        # # TODO: сохранять данные списком или брать их из соседней модели?
+        # if df is None:
+        #     df = pd.DataFrame(columns=self.columns)
+        # super(ShipmentMapModel, self).__init__(df)
+
+    def data(self, index, role=Qt.DisplayRole):
+        if not index.isValid():
+            return
+
+        if role == Qt.DisplayRole:
+            return str(self._data.iloc[index.row(), index.column()])        # TODO: + weight if notna
+        if role == Qt.TextAlignmentRole:
+            return Qt.AlignCenter
+        # if (self.model_type == 'map') and (role == Qt.TextWordWrap):
+        #     return True
+        # if role == Qt.FontRole:
+        #     return QFont('Courier New')
+
+    # def import_from(self, df: pd.DataFrame):
+    #     samples = df[self.code_column]
+    #     array, indexes = [], []
+    #     for index in range(0, samples.size, self.box_options.columns):
+    #         row = (index // self.box_options.columns) % self.box_options.rows + 1
+    #         array.append(samples.values[index:index + self.box_options.columns])
+    #         indexes.append(row)
+    #         # add separators
+    #         if row == self.box_options.rows:
+    #             array.extend([[''] * self.box_options.columns] * self.box_options.separator)
+    #             indexes.extend([''] * self.box_options.separator)
+    #     self._data = pd.DataFrame(array, index=indexes, columns=self.columns).fillna('')
--- spa_ui.py
+++ spa_ui.py
@@ -7,9 +7,8 @@ from PyQt5 import QtWidgets, uic
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import QFileDialog, QHeaderView
-from string import ascii_lowercase
 
-from shipment_models import ShipmentModel
+from shipment_models import ShipmentMapModel, ShipmentListModel, ShipmentModel
 
 ShipmentListColumns = namedtuple('ShipmentListColumns', 'code st0 st1 st2 st3 st4')
 
@@ -19,11 +18,6 @@ class ShipmentPackingAssistantUI(QtWidgets.QMainWindow):
         super(ShipmentPackingAssistantUI, self).__init__()
         uic.loadUi('ui/spa2.ui', self)
         # general settings
-        self.box_columns = 9
-        self.box_rows = 9
-        self.separator = 2
-        self.list_columns = ShipmentListColumns('Код', 'st0', 'st1', 'st2', 'st3', 'st4')
-        self.map_columns = [''] + list(ascii_lowercase[:self.box_columns])
         self.font = QFont('Courier New')
 
         # initialize components
@@ -44,31 +38,27 @@ class ShipmentPackingAssistantUI(QtWidgets.QMainWindow):
         self.work_button.clicked.connect(self.debug_action)
 
         # initialize models for tables
-        # samples_df = pd.DataFrame('0000000T0(00)', index=[3, 4, 5], columns=list(self.list_columns))
-        samples_df = pd.DataFrame(index=[], columns=list(self.list_columns))
-        self.list_model = ShipmentModel(samples_df, 'list')
-        self.list_view.setModel(self.list_model)
+        self.shipment = ShipmentModel()
+        self.list_view.setModel(self.shipment.list_model)
+        self.map_view.setModel(self.shipment.map_model)
+        # dbg_samples_df = pd.DataFrame('0000000T0(00)',
+        #                               index=np.arange(0, 20),
+        #                               columns=['Код', 'st0', 'st1', 'st2', 'st3', 'st4'])
+        # self.list_model = ShipmentListModel()
+        # self.list_view.setModel(self.list_model)
 
-        # self.map_df = pd.DataFrame('0000000T0(00) 0.55', index=[3, 4, 5], columns=self.map_columns)
-        map_df = pd.DataFrame(index=[], columns=self.map_columns)
-        self.map_model = ShipmentModel(map_df, 'map')
-        self.map_view.setModel(self.map_model)
+        # dbg_map_df = pd.DataFrame('0000000T0(00) 0.55', index=np.arange(0, 20), columns=list('abcdefghi'))
+        # self.map_model = ShipmentMapModel()
+        # self.map_view.setModel(self.map_model)
 
         # setup components look
-        self.list_view.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
         self.list_view.verticalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
         self.list_view.horizontalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
         self.list_view.horizontalHeader().setSectionResizeMode(0, QHeaderView.Stretch)
-        # self.list_view.horizontalHeader().setOffset(2)
-        # self.list_view.setContentsMargins(2, 2, 2, 2)
-        self.list_view.horizontalHeader().setMinimumSectionSize(24)
 
         self.map_view.verticalHeader().setSectionResizeMode(QHeaderView.ResizeToContents)
         self.map_view.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-        self.map_view.horizontalHeader().setSectionResizeMode(0, QHeaderView.ResizeToContents)
-        self.map_view.horizontalHeader().setMinimumSectionSize(32)
         self.map_view.setFont(self.font)
-        self.map_view.setWordWrap(True)
 
         # show form
         self.show()
@@ -82,32 +72,17 @@ class ShipmentPackingAssistantUI(QtWidgets.QMainWindow):
             return
         self.status_bar.showMessage(f'Opening file "{list_file.selectedFiles()[0]}"')
         file_df = pd.read_excel(list_file.selectedFiles()[0])
-        # if target columns was not found --> exit
-        if any([col not in file_df.columns for col in self.list_columns]):
-            self.status_bar.showMessage(f'ERROR! Cannot find column(s) "{self.list_columns}" in selected file!')
-            return
-        # get list of samples with its placements and put it to view
-        samples_df = file_df[list(self.list_columns)]
-        self.list_model = ShipmentModel(samples_df, 'list')
-        self.list_view.setModel(self.list_model)
 
-        # get amount of required rows in map for samples
-        max_rows = int(np.ceil(samples_df.shape[0] / self.box_columns))
+        self.shipment.load(file_df)
+        # self.list_model.import_from(file_df)
+        self.shipment.list_model.layoutChanged.emit()
 
-        # convert list to map
-        samples = file_df[self.list_columns.code]
-        arr = [['', *samples.values[i:i + self.box_columns]] for i in range(0, samples.size, self.box_columns)]
-        map_df = pd.DataFrame(arr, index=np.arange(0, max_rows), columns=self.map_columns)
-        # generate first map column
-        map_df.iloc[:, 0] = [n % self.box_columns for n in range(1, max_rows + 1)]
-        # insert separators
-        # TODO: insert separators
-
-        self.map_model = ShipmentModel(map_df.fillna(''), 'map')
-        self.map_view.setModel(self.map_model)
+        # self.map_model.import_from(file_df)
+        self.shipment.map_model.layoutChanged.emit()
 
     def debug_action(self):
-        self.map_model.df.iloc[:, 1:] += ' 0.55'
+        cond = (self.map_model.df.index != '')
+        self.map_model.df[cond] += ' 0.55'
         self.map_model.layoutChanged.emit()
 
 
